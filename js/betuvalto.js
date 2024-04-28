90a27d8408753db695ef1932eded0cc90fa27df8
MrZalan <168220746+MrZalan@users.noreply.github.com>
2024-04-28 14:01:34 +0200

    Update betuvalto.js

--- js/betuvalto.js
+++ js/betuvalto.js
@@ -3,9 +3,9 @@ function toggleTheme() {
 
     var theme = document.getElementsByTagName('link')[0]; 
 
-    if (theme.getAttribute('href') == 'css/alap.css') { 
-        theme.setAttribute('href', 'css/nagybetus.css'); 
+    if (theme.getAttribute('href') == './css/alap.css') { 
+        theme.setAttribute('href', './css/nagybetus.css'); 
     } else { 
-        theme.setAttribute('href', 'css/alap.css'); 
+        theme.setAttribute('href', './css/alap.css'); 
     } 
 } 
