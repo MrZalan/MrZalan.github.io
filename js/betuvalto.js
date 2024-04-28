894b0010d8b73d81ced60b805362e6919d921612
MrZalan <wwavey@inf.elte.hu>
2024-04-28 12:27:32 +0200

    asd

---
+++ js/betuvalto.js
@@ -0,0 +1,11 @@
+
+function toggleTheme() { 
+
+    var theme = document.getElementsByTagName('link')[0]; 
+
+    if (theme.getAttribute('href') == '../css/alap.css') { 
+        theme.setAttribute('href', '../css/nagybetus.css'); 
+    } else { 
+        theme.setAttribute('href', '../css/alap.css'); 
+    } 
+} 
